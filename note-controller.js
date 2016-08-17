3eb6683da9daed53b91232423c01a76243acfe75
hannako <jessjones1@gmail.com>
2016-08-17 20:14:49 +0100

    challenge 4

---
+++ note-controller.js
@@ -0,0 +1,9 @@
+// Add code that gets hold of the app div element in your index.html page.
+//console.log the element to be sure you've got it.
+//  Add code that can change the app greeting from hello to howdy.
+//  Visit http://localhost:8080 and check you can see howdy.
+
+
+
+
+document.getElementById("app").innerHTML = "howdy";
