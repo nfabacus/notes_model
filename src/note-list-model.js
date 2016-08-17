519356f2a12eeb5391ff5f1e2c79ad2de10547c0
alwinsolanky <alwinsolanky@gmail.com>
2016-08-17 15:54:11 +0100

    list of notes

---
+++ src/note-list-model.js
@@ -0,0 +1,12 @@
+function NoteList(){
+  this.notes = [];
+}
+NoteList.prototype.displayList = function(){
+  console.log("display the list");
+  return this.notes;
+};
+
+NoteList.prototype.addNote = function(note){
+  this.notes.push(new Note(note));
+  console.log("Note added!");
+}
