517f67e39835d6094d63854ee57a2787de02e5e4
noby <n.fujioka@abacusmaths.info>
2016-08-18 20:43:06 +0100

    ex10: adds a form.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var list, controller;
 
 list = new noteList();
-list.createNote("Favourite drink: seltzer");
+list.createNote("Note1: Kombucha biodiesel gochujang, bushwick pickled leggings flexitarian bitters 90's scenester single-origin coffee trust fund cold-pressed hammock dreamcatcher.");
+list.createNote("Note2: Brunch kogi fingerstache, selvage listicle pork belly artisan try-hard taxidermy before they sold out.");
+list.createNote("Note3: Knausgaard mlkshk meditation jean shorts, pop-up marfa skateboard selvage dreamcatcher biodiesel truffaut flexitarian sustainable pinterest.");
+
+
 
 controller = new noteController(list);
 controller.addToIndex();
